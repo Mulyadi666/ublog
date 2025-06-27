621a16f6f21e06fe81a69966f771401eb3cd3ae9
Adi Mulyadi <adimulyadi@email.com>
2025-06-27 20:43:14 +0700

    try vercel 2

--- app/api/index.php
+++ app/api/index.php
@@ -1,2 +1,16 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+// Autoload Composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// Load Laravel App
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Handle HTTP request
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$request = Illuminate\Http\Request::capture();
+$response = $kernel->handle($request);
+
+$response->send();
+
+$kernel->terminate($request, $response);
